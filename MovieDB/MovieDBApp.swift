035676072a5323d1c996b780126108cd769981fe
MaiHassen <mai.hassen@squadio.com>
2024-04-20 12:02:42 +0200

    Initial Commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Mai Hassen on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
