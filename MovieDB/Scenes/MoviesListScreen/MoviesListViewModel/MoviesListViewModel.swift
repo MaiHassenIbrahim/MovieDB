a033cc5ee2e8c8cec1425ec294a5fc0c889878b8
MaiHassen <mai.hassen@squadio.com>
2024-04-23 13:22:11 +0200

    Work Done: - Add Search bar - Add Sort button to sort by Movie Title

--- MovieDB/Scenes/MoviesListScreen/MoviesListViewModel/MoviesListViewModel.swift
+++ MovieDB/Scenes/MoviesListScreen/MoviesListViewModel/MoviesListViewModel.swift
@@ -10,8 +10,16 @@ import Combine
 
 class MoviesListViewModel: ObservableObject {
     @Published var movies: [Movie] = []
+    @Published var allMovies: [Movie] = []
     @Published var apiError: APIError?
     @Published var isLoading = false
+    var isMovieApiCalled = true
+    var searchText: String = "" {
+        didSet {
+            self.search()
+        }
+    }
+    
     private var page = 1
     private let movieRepository: MoviesListRepository
     private var cancellables: Set<AnyCancellable> = []
@@ -21,6 +29,7 @@ class MoviesListViewModel: ObservableObject {
     }
     
     func fetchMovies() {
+        isMovieApiCalled = true
         movieRepository.fetchMovies(page: 1)
             .receive(on: DispatchQueue.main)
             .sink { completion in
@@ -30,12 +39,14 @@ class MoviesListViewModel: ObservableObject {
                 }
             } receiveValue: { movies in
                 self.movies = movies
+                self.allMovies = movies
             }
             .store(in: &cancellables)
     }
     func loadMore() {
-            guard !isLoading else { return }
-            
+        
+            guard !isLoading && isMovieApiCalled else { return }
+        isMovieApiCalled = true
             isLoading = true
         movieRepository.fetchMovies(page: self.page)
             .receive(on: DispatchQueue.main)
@@ -47,11 +58,31 @@ class MoviesListViewModel: ObservableObject {
                 }
             } receiveValue: { movies in
                 self.movies.append(contentsOf: movies) 
+                self.allMovies.append(contentsOf: movies)
             }
             .store(in: &cancellables)
         }
     
-
+    func sortList() {
+        movies.sort(by: {$0.title.lowercased() < $1.title.lowercased()})
+    }
+    
+    func search() {
+        isMovieApiCalled = false
+        if searchText.trimmingCharacters(in: .whitespacesAndNewlines) != "" {
+            let searchedList = self.movies.compactMap({ item in
+                if (item.title.lowercased().contains(searchText.lowercased())) {
+                    return item
+                }
+                return nil
+            })
+            self.movies = searchedList
+         
+        } else {
+            self.movies = allMovies
+            isMovieApiCalled = true
+        }
+    }
     func launchMovieDetailsData() -> MovieDetailsViewModel{
         let apiService = MovieAPIService()
         let movieRepository = MovieDetailsDataRepository(apiService: apiService)
--- MovieDB/Scenes/MoviesListScreen/MoviesListiew/MoviesListView.swift
+++ MovieDB/Scenes/MoviesListScreen/MoviesListiew/MoviesListView.swift
@@ -17,60 +17,79 @@ struct MoviesListView: View {
         NavigationView {
             VStack {
                 if movieListViewModel.movies.isEmpty {
-                    ProgressView()
-                } else {
-                 
-                     InfiniteList(data: $movieListViewModel.movies,
-                                          isLoading: $movieListViewModel.isLoading,
-                                          loadingView: ProgressView(),
-                                          loadMore: movieListViewModel.loadMore) { movie in
-                   
-                                if let imageUrl = movie.posterURL {
-                                    Button(action: {
-                                    selectedMovie = movie
-                                    }) {
-                                     
-                                        HStack(spacing: 0){
-                                                WebImage(url: imageUrl)
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fit)
-                                                    .frame(height: 170)
-                                                    .cornerRadius(10)
-                                                    .padding(.vertical, 25)
-                                                    .padding(.leading, 25)
-                                                VStack(alignment: .leading){
-                                                    Spacer().frame(height: 10)
-                                                    Text(movie.title)
-                                                        .font(.system(size: 20, weight: .bold))
-                                                        .padding(.top,20)
-                                                        .padding(.leading,4)
-                                                        .foregroundColor(.white)
-                                                        .multilineTextAlignment(.leading)
-                                                    Text(movie.year)
-                                                        .font(.system(size: 15, weight: .semibold))
-                                                        .foregroundColor(.white)
-                                                        .padding(4)
-                                                    Spacer()
-                                                }.padding()
-                                                Spacer()
-                                            }
-                                                .background(.black)
-                                                .cornerRadius(18)
-                                                .accessibilityIdentifier("MovieListItem")
-                                        
-                                    }
-                                    
-                                }
+                    if movieListViewModel.isMovieApiCalled {
+                        ProgressView()
+                    } else {
+                        Text("Empty")
                     }
-                        }
+                } else {
                     
+                    InfiniteList(data: $movieListViewModel.movies,
+                                 isLoading: $movieListViewModel.isLoading,
+                                 loadingView: ProgressView(),
+                                 loadMore: movieListViewModel.loadMore) { movie in
+                        
+                        if let imageUrl = movie.posterURL {
+                            Button(action: {
+                                selectedMovie = movie
+                            }) {
+                                
+                                HStack(spacing: 0){
+                                    WebImage(url: imageUrl)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(height: 170)
+                                        .cornerRadius(10)
+                                        .padding(.vertical, 25)
+                                        .padding(.leading, 25)
+                                    VStack(alignment: .leading){
+                                        Spacer().frame(height: 10)
+                                        Text(movie.title)
+                                            .font(.system(size: 20, weight: .bold))
+                                            .padding(.top,20)
+                                            .padding(.leading,4)
+                                            .foregroundColor(.white)
+                                            .multilineTextAlignment(.leading)
+                                        Text(movie.year)
+                                            .font(.system(size: 15, weight: .semibold))
+                                            .foregroundColor(.white)
+                                            .padding(4)
+                                        Spacer()
+                                    }.padding()
+                                    Spacer()
+                                }
+                                .background(.black)
+                                .cornerRadius(18)
+                                .accessibilityIdentifier("MovieListItem")
+                                
+                            }
+                            
+                        }
+                    }
+                }
+                
                 
             }
+            .navigationBarItems(trailing:
+                                    HStack {
+                Button(action: {
+                    // sort by title
+                    movieListViewModel.sortList()
+                }, label: {
+                    Image(systemName: "arrow.up.arrow.down").resizable()
+                    Text("Sort")
+                    
+                    
+                }).tint(.purple)
+                
+            })
+            .searchable(text: $movieListViewModel.searchText, prompt: Text("Search text..."))
+            
             .navigationTitle("Movies")
             .sheet(item: $selectedMovie) { movie in
                 // go to details screen
                 MovieDetailsView(movieDetailsViewModel: movieListViewModel.launchMovieDetailsData(), movieId: movie.id)
-         
+                
             }
             .onAppear {
                 movieListViewModel.fetchMovies()
